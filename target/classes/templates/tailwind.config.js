bdbf71f13aeafbdedd9580834248ade4992fdfbf
Fletcher032 <95692933+Fletcher032@users.noreply.github.com>
2024-01-11 19:45:40 +0800

    About html & Tailwind CSS Setup

---
+++ target/classes/templates/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				clifford: "#da373d",
+				buttonLeft: "#e1c2f0",
+				buttonRight: "#6897fa",
+				white: "#ffffff",
+				// background white color: bg-slate-50
+			},
+		},
+		fontFamily: {
+			// add your fonts here
+			montserrat: ['Montserrat'],
+			comfortaa: ["Comfortaa", "sans-serif"],
+			nunito: ["Nunito", "sans-serif"],
+			outfit: ["Outfit", "sans-serif"],
+			playfair: ["Playfair Display", "serif"],
+			poppins: ["Poppins", "sans-serif"],
+			raleway: ["Raleway", "sans-serif"],
+			roboto: ["Roboto", "sans-serif"],
+			lato: ['Lato'],
+			garamond: ['Garamond'],
+		},
+	},
+};
