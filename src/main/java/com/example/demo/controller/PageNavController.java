4301b0f8145f1e8480848069e50cc92afbbafa64
hee-sy <88873247+hee-sy@users.noreply.github.com>
2024-01-13 00:02:28 +0800

    PageNavController mappings to pages

--- src/main/java/com/example/demo/controller/PageNavController.java
+++ src/main/java/com/example/demo/controller/PageNavController.java
@@ -6,44 +6,58 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageNavController {
-	
+
 	@GetMapping("/home")
 	public String homePage(Model model) {
 		return "Home";
+	}
+
+	@GetMapping("/product")
+	public String productPage(Model model) {
+		return "Product";
 	}
 
 	@GetMapping("/about")
 	public String aboutPage(Model model) {
 		return "About";
 	}
-	
-	
+
+	@GetMapping("/forCustomer")
+	public String forCustomerPage(Model model) {
+		return "ForCustomer";
+	}
+
+	@GetMapping("/contactUs")
+	public String contactUsPage(Model model) {
+		return "ContactUs";
+	}
+
 	// Check for header & footer component cause idk how to open jsp
 	@GetMapping("/custHeader")
 	public String headerBar(Model model) {
 		return "CustHeader";
 	}
-	
+
 	@GetMapping("/custFooter")
 	public String footerBar(Model model) {
 		return "CustFooter";
 	}
-	
+
 	@GetMapping("/prodDetails")
 	public String productDetails(Model model) {
 		return "ProductDetails";
 	}
-	
+
 	@GetMapping("/orderreq1")
 	public String orderReq1(Model model) {
 		return "OrderRequest1";
 	}
-	
+
 	@GetMapping("/orderreq2")
 	public String orderReq2(Model model) {
 		return "OrderRequest2";
 	}
-	
+
 	@GetMapping("/orderreq3")
 	public String orderReq3(Model model) {
 		return "OrderRequest3";
