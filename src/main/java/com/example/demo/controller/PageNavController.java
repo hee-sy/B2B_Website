43b213c10403f55c4a7e0508cbe8907ea9802508
Ryan Ho <yu.wae@hotmail.com>
2024-01-14 17:28:18 +0800

    adjustments to contactus form

--- src/main/java/com/example/demo/controller/PageNavController.java
+++ src/main/java/com/example/demo/controller/PageNavController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.Order;
 import com.example.demo.entity.Product;
@@ -55,7 +56,7 @@ public class PageNavController {
 		return "ContactUs";
 	}
 
-	@GetMapping("/contactUsResponse")
+	@PostMapping("/contactUsResponse")
 	public String contactUsResponsePage(Model model) {
 		return "ContactUsResponse";
 	}
