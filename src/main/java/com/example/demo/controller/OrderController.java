cbd74ba1a0fa27dc926a374f8f84494f5ed0cb04
hee-sy <88873247+hee-sy@users.noreply.github.com>
2024-01-14 20:02:38 +0800

    order req backend

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Order;
 import com.example.demo.entity.Product;
@@ -31,16 +32,20 @@ public class OrderController {
 
 	// from order request 1
 	@PostMapping("/order/{id}")
-	public String saveOrder(@PathVariable Long id, @ModelAttribute("order1") Order order, Model model) {
+	public String saveOrder(@RequestParam String quantity_frm_orderreq1, @RequestParam String total_frm_orderreq1,
+			@PathVariable Long id, @ModelAttribute("order1") Order order, Model model) {
 		Product prod2 = new Product();
 		prod2 = productService.getProductById(id);
+
+		order.setOrderQuantity(Integer.parseInt(quantity_frm_orderreq1));
+		order.setOrderCost(Double.parseDouble(total_frm_orderreq1));
 
 		order.setOrderProduct(prod2.getProdName());
 		long mil = System.currentTimeMillis();
 		java.sql.Date date = new java.sql.Date(mil);
 		order.setOrderCreatedAt(date);
 
-		double cost = order.getOrderCost();
+		double cost = order.getOrderCost(); // cannot get
 
 		order.setOrderStatus(false);
 
